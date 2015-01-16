0fa9790eafe4d7b7802dcfc262f90c02b6c60a97
toddlerya <toddlerya@gmail.com>
2015-01-16 09:42:58 +0800

    Update 5-3.py

--- Chapter_5/5-3.py
+++ Chapter_5/5-3.py
@@ -1,6 +1,22 @@
 #!/usr/bin/env python
 #coding: utf-8
  
+"""
+5-3 标准类型运算符. 写一段脚本,输入一个测验成绩,根据下面的标准,输出他的评分
+
+成绩(A-F)。
+
+A: 90–100
+
+B: 80–89
+
+C: 70–79
+
+D: 60–69
+
+F: <60
+"""
+
 def get_score():       
     global score
     score = raw_input('Please input the score(1~100): ')
