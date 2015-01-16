124dd59d659a9167c63cd498b6bdccc01f752ea8
toddlerya <toddlerya@gmail.com>
2015-01-16 09:41:23 +0800

    Create 5-2.py
    5-2：运算符
    
    (a) 写一个函数,计算并返回两个数的乘积
    
    (b) 写一段代码调用这个函数,并显示它的结果

---
+++ Chapter_5/5-2.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+#coding: utf-8
+ 
+'(a)写一个函数，计算并返回两个数的乘积'
+'(b)写一段代码调试这个函数，并显示它的结果'
+ 
+def multiply(num_1, num_2):
+    return (num_1 * num_2)
+ 
+ 
+print multiply(3, 5)        #output---->15
+print multiply(3.2, -12）   #output---->-38.4
